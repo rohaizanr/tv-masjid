69610f642f793b007738a7898e59388f2a16c6a2
izandotnet <rohaizanr@gmail.com>
2022-03-08 21:30:29 +0800

    Marquee annoucement from file

--- src/Component/Announcement/Announcement.js
+++ src/Component/Announcement/Announcement.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import Marquee from "react-fast-marquee";
+import { getAnnouncements } from '../../utils/applicationSettings'
 
 class Announcement extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            text : this.getAnnouncementAsString()
         };
       }
     
+    getAnnouncementAsString() {
+        // var result = "";
+        var temp = getAnnouncements();
+        return temp.join(" ------------------------------------ ");
+    }
     render() {
         return (
             <div className="container-fluid bg-danger bg-gradient text-white position-absolute bottom-0 end-0">
-                Scrolling announcement here
+                <Marquee gradientColor="[255,0,0]" speed="100">
+                    { this.state.text }
+                </Marquee>
             </div>
         );
     }
